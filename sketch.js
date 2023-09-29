e9525693c5ac232f784971f800b5b1dfc3ecb688
Shashwat Mishra <144282119+smx64@users.noreply.github.com>
2023-09-29 00:21:02 -0400

    Code V2

--- sketch.js
+++ sketch.js
@@ -1,39 +1,68 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
-  fill(255);
 }
 
-let timer = 100;
+function createRect()
+{
+  rect(xpos,0,width/60,height-100);
+  xpos+=width/60; 
+}
+
+let timer = 1000;
 let eventChange = timer;
 let xpos = 0;
 let hoursElapsed = 0;
 let minutesElapsed = 0;
+let seasonName = 'SPRING';
 
 function draw() {
   strokeWeight(1);
   if(millis()>eventChange)
   {
-    if(xpos<width)
+    if(xpos>=0 && xpos<width/4)
     {
-      rect(xpos,0,width/60,height-100);
-      xpos+=width/60;
-      if(xpos>=width)
+      fill('#46EB3B');
+      createRect();
+    }
+    if(xpos>=width/4 && xpos<width/2)
+    {
+      fill("#FFE945");
+      createRect();
+      seasonName = 'SUMMER';
+    }
+    if(xpos>=width/2 && xpos<3*width/4)
+    {
+      fill("#FF6D24");
+      createRect();
+      seasonName = 'FALL';
+    }
+    if(xpos>=3*width/4 && xpos<width)
+    {
+      fill("#2470FF");
+      createRect();
+      seasonName = 'WINTER';
+    }
+    if(xpos>=width)
+    {
+      background(0);
+      xpos=0;
+      minutesElapsed++;
+      seasonName = 'SPRING';
+      if(minutesElapsed%60==0)
       {
-        background(0);
-        xpos=0;
-        minutesElapsed++;
-        if(minutesElapsed%60==0)
-        {
-          hoursElapsed++;
-          minutesElapsed = 0;
-        }
+        hoursElapsed++;
+        minutesElapsed = 0;
       }
     }
     eventChange = millis() + timer;
   }
+  fill(0);
+  strokeWeight(0);
+  rect(0,height-100,width,height-100);
   fill(255);
   textSize(32);
-  text('Hours Elapsed: ' +hoursElapsed, width/4, height-40);
-  text('Minutes Elapsed: '+minutesElapsed, width/1.75, height-40);
+  text(seasonName, 30, height-40);
+  text('Hours Elapsed: ' +hoursElapsed, width/2.5, height-40);
+  text('Minutes Elapsed: '+minutesElapsed, width/1.25, height-40);
 }
